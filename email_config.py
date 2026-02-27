a07faa0b356074c931f894fc4dfedbdb72af07d4
Veer Mehta <e1357046@u.nus.edu>
2026-02-28 07:37:14 +0800

    Add dependencies

--- email_config.py
+++ email_config.py
@@ -1,4 +1,10 @@
-EMAIL_ADDRESS = "veermehta270@gmail.com"
-EMAIL_PASSWORD = "rxzn dwoj adqn sglk"
+# email_config.py
+
+import streamlit as st
+
+# Secure credentials from Streamlit secrets
+EMAIL_ADDRESS = st.secrets["EMAIL_ADDRESS"]
+EMAIL_PASSWORD = st.secrets["EMAIL_PASSWORD"]
+
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
